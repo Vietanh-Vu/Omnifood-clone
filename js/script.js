be12e57d463299f52d694c3fad106c6014d55882
Vietanh-Vu <vuvietanh993@gmail.com>
2022-12-17 22:19:07 +0700

    add animation to section

--- js/script.js
+++ js/script.js
@@ -1,94 +1,113 @@
 ///////////////////////////////////////////////////////////
-// Fixing year in copyright 
-const yearEl = document.querySelector('.year')
-const currentYear = new Date().getFullYear()
-yearEl.textContent = currentYear
-
+// Fixing year in copyright
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
-const btnMobileNavEl = document.querySelector('.btn-mobile-nav')
-const headerEl = document.querySelector('.header')
+const btnMobileNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnMobileNavEl.addEventListener('click', function () {
-    headerEl.classList.toggle('nav-open')
-})
+btnMobileNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
-const allLinks = document.querySelectorAll('a:link')
+const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
-    link.addEventListener('click', function (e) {
-        e.preventDefault()
-        const href = link.getAttribute('href')
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-        // scroll back to top 
-        if (href === '#') {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-            })
-        }
+    // scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
 
-        // Scroll to section
-        if (href !== '#' && href.startsWith('#')) {
-            const sectionEl = document.querySelector(href)
-            sectionEl.scrollIntoView({
-                behavior: 'smooth'
-            })
-        }
+    // Scroll to section
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
 
-        // Close mobile navigation
-        if (link.classList.contains('main-nav-link')) {
-            headerEl.classList.toggle('nav-open')
-        }
-    })
-})
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
 
 ///////////////////////////////////////////////////////////
-// Sticky navigation 
-const sectionHeroEl = document.querySelector('.section-hero')
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
-    function (entries) {
-        const ent = entries[0]
-        if (ent.isIntersecting === false) {
-            document.body.classList.add('sticky')
-        }
-        if (ent.isIntersecting === true) {
-            document.body.classList.remove('sticky')
-        }
-    },
-    {
-        // in the viewport
-        root: null,
-        threshold: 0,
-        rootMargin: '-80px',
-    })
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
 
-obs.observe(sectionHeroEl)
+obs.observe(sectionHeroEl);
 
+///////////////////////////////////////////////////////////
+// Section animation
 
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  //   if (!entry.isIntersecting) return;
+  //   entry.target.classList.remove("section--hidden");
+  if (entry.isIntersecting) entry.target.classList.remove("section--hidden");
+  else entry.target.classList.add("section--hidden");
+  //   observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "column";
-    flex.style.rowGap = "1px";
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
 
-    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
 
-  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
